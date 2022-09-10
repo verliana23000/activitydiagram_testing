7a0e4fa14dcf271cdeb9a143a09cdb2c0a24a23c
Verliana NS <verliananss23000@gmail.com>
2022-09-10 09:17:31 +0700

    lassst

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,120 +9,120 @@
   <meta content="" name="keywords">
 
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,600,600i,700,700i" rel="stylesheet">
-
   <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-
   <link href="assets/css/style.css" rel="stylesheet">
 
 </head>
 
 <body>
-@php
+  @php
     $awal=array();
     $tujuan=array();
     $nama=array();
     $cabang=array();
     $buntu=array();
     $start=0;
+  @endphp
 
-@endphp
   <!-- ======= Header ======= -->
-  <header id="header" class="d-flex align-items-center">
-    <div class="container d-flex flex-column align-items-center">
 
-      <span><h1>Rekomendasi Kasus Uji</h1></span>
-        <h1>Diagram Aktivitas</h1>
-          <h2>Skripsi</h2>
+    <header id="header" class="d-flex align-items-center">
+      <div class="container d-flex flex-column align-items-center">
 
-      <div class="subscribe">
-        <h4>Upload File *ptl</h4>
-        <form action="{{route('upload.store')}}" method="post" enctype="multipart/form-data">
-          @csrf 
-          <div class="form-control">
-            <input type="file" name="file" accept=".ptl" required>
-          </div>
-          <button type="submit" class="btn btn-primary">Proses</button>
-        </form>
+        <span><h1>Rekomendasi Kasus Uji</h1></span>
+          <h1>Diagram Aktivitas</h1>
+            <h2>Skripsi</h2>
+
+        <div class="subscribe">
+          <h4>Upload File *ptl</h4>
+            <form action="{{route('upload.store')}}" method="post" enctype="multipart/form-data">
+              @csrf 
+              <div class="form-control">
+                <input type="file" name="file" accept=".ptl" required>
+              </div>
+                <button type="submit" class="btn btn-primary">Proses</button>
+            </form>
+        </div>
       </div>
-    </div>
-  </header>
+    </header>
+
   <!-- End #header -->
+
     <!-- ======= About Us Section ======= -->
     <section id="about" class="about">
       <div class="container">
         <div class="section-title">
           @if(session()->has('success'))
-          {{ session()->get('success')}}
-          
-          <?php
-            $cek=1;
-            $nama_file=session()->get('success');
-            $data = file('file/'.$nama_file);
-            $hasil = array();
-            $counter = 0;
-            $no=1;
+            {{ session()->get('success')}}
+
+            <?php
+              $cek=1;
+              $nama_file=session()->get('success');
+              $data = file('file/'.$nama_file);
+              $hasil = array();
+              $counter = 0;
+              $no=1;
                 
-            for ($i = 0; $i < count($data); $i++){
-            
-            if( (str_contains($data[$i], '(object ActivityStateView ')) ){ // Activity State
-                $activity_state = "ActivityState";
-                $activity_name = str_replace('"', '', trim($data[$i],"\t     (object ActivityStateView "));
-                $activity_name = str_replace('@', '', trim($activity_name));
-                $id = substr($activity_name,-2);
-                $activity_name = preg_replace('/[0-9]+/', '', $activity_name);
-                $counter++;
+              for ($i = 0; $i < count($data); $i++){
+                // Activity State
+                if( (str_contains($data[$i], '(object ActivityStateView ')) ){ 
+                    $activity_state = "ActivityState";
+                    $activity_name = str_replace('"', '', trim($data[$i],"\t     (object ActivityStateView "));
+                    $activity_name = str_replace('@', '', trim($activity_name));
+                    $id = substr($activity_name,-2);
+                    $activity_name = preg_replace('/[0-9]+/', '', $activity_name);
+                    $counter++;
   
-                $hasil[$counter] = array(
-                    "State" => $activity_state,
-                    "Activity Name" => $activity_name,
-                    "ID" => $id,
-                    "Dependency" => ""
-                );
-            }
+                    $hasil[$counter] = array(
+                        "State" => $activity_state,
+                        "Activity Name" => $activity_name,
+                        "ID" => $id,
+                        "Dependency" => ""
+                    );
+                  }
+                    // DecisionState
+                  elseif( (str_contains($data[$i], '(object DecisionView ')) ){
+                    $activity_state = "DecisionState";
+                    $activity_name = str_replace('"', '', trim($data[$i],"\t     (object DecisionView "));
+                    $activity_name = str_replace('@', '', trim($activity_name));
+                    $id = substr($activity_name,-2);
+                    $activity_name = preg_replace('/[0-9]+/', '', $activity_name);
+                    $counter++;
+
+                    $hasil[$counter] = array(
+                        "State" => $activity_state,
+                        "Activity Name" => $activity_name,
+                        "ID" => $id,
+                        "Dependency" => ""
+                    );
+                  }
   
-            // DecisionState
-            elseif( (str_contains($data[$i], '(object DecisionView ')) ){
-                $activity_state = "DecisionState";
-                $activity_name = str_replace('"', '', trim($data[$i],"\t     (object DecisionView "));
-                $activity_name = str_replace('@', '', trim($activity_name));
-                $id = substr($activity_name,-2);
-                $activity_name = preg_replace('/[0-9]+/', '', $activity_name);
-                $counter++;
-  
-                $hasil[$counter] = array(
-                    "State" => $activity_state,
-                    "Activity Name" => $activity_name,
-                    "ID" => $id,
-                    "Dependency" => ""
-                );
-            }
-  
-            //StartState & EndState
-            elseif( (str_contains($data[$i], '(object StateView ')) ) {
-                $activity_state = str_replace('"', '', trim($data[$i],"\t     (object StateView "));
-                $activity_state = str_replace('@', '', trim($activity_state));
-                $activity_state = str_replace('$', '', trim($activity_state));
-                $activity_state = preg_replace('/UNNAMED/i', '', $activity_state);
-                $activity_state = preg_replace('/[0-9]+/','', $activity_state);
-                $activity_state = str_replace(' ', '', trim($activity_state));
-                
-                $activity_name = str_replace('"', '', trim($data[$i],"\t     (object StateView "));
-                $activity_name = preg_replace('/StartState/', '',$activity_name);
-                $activity_name = preg_replace('/EndState/', '', $activity_name);
-                $activity_name = str_replace('@', '', trim($activity_name));
-                $id = substr($activity_name,-2);
-                $activity_name = preg_replace('/[0-9]+/','', $activity_name);
-                $counter++;
-  
-                $hasil[$counter] = array(
-                    "State" => $activity_state,
-                    "Activity Name" => $activity_name,
-                    "ID" => $id,
-                    "Dependency" => ""
-                );
-            }
-        }
+                  //StartState & EndState
+                  elseif( (str_contains($data[$i], '(object StateView ')) ) {
+                    $activity_state = str_replace('"', '', trim($data[$i],"\t     (object StateView "));
+                    $activity_state = str_replace('@', '', trim($activity_state));
+                    $activity_state = str_replace('$', '', trim($activity_state));
+                    $activity_state = preg_replace('/UNNAMED/i', '', $activity_state);
+                    $activity_state = preg_replace('/[0-9]+/','', $activity_state);
+                    $activity_state = str_replace(' ', '', trim($activity_state));
+              
+                    $activity_name = str_replace('"', '', trim($data[$i],"\t     (object StateView "));
+                    $activity_name = preg_replace('/StartState/', '',$activity_name);
+                    $activity_name = preg_replace('/EndState/', '', $activity_name);
+                    $activity_name = str_replace('@', '', trim($activity_name));
+                    $id = substr($activity_name,-2);
+                    $activity_name = preg_replace('/[0-9]+/','', $activity_name);
+                    $counter++;
+
+                    $hasil[$counter] = array(
+                        "State" => $activity_state,
+                        "Activity Name" => $activity_name,
+                        "ID" => $id,
+                        "Dependency" => ""
+                    );
+                  }
+                }
   
             /** 
             * Looping Kedua
@@ -162,35 +162,60 @@
                           <tr>
                             <td>{{$no++}}</td> 
                             <td>{{$value['State']}}</td>
+                            @if($value['Activity Name']=='$UNNAMED$ ')
+                              @php
+                                $unname=$value['State'];
+                                $a = substr($unname,0,-5);
+                              @endphp
+                                <td>{{$a}}</td>
+                            @else
                             <td>{{$value['Activity Name']}}</td>
+                            @endif
                             <td>{{$value['ID']}}</td>
                             <td>{{$value['Dependency']}}</td>                
                           </tr>
                           @endforeach
                         </table>
                       </div>
+
+                      <br><br>
                       <div class="col-10" align="center">
                         <span><h2 style="color:black ">Activity Dependency Graph</h2></span>
                         <div style="color: black">
+                          <table  class = " table-bordered">
+                            <tr>
+                              <td>Dependency</td>
+                              <td>ID</td>
+                            </tr>
                           @foreach ($hasil as $key => $value)
                               <?
                                 $dependency = trim($value['Dependency'], " \n");
                                 $dependency = str_replace('\n', '', trim($dependency));
                                 $id = trim($value['ID'], " \n");
                               ?>
-                              {{ ($value['Dependency']. "->" .$value['ID']) }}<br>
-                              
+                              <tr>
+                                <td>{{ $value['Dependency'] }}</td>
+                                <td>{{ $value['ID'] }}</td>
+                              </tr>
+
                               @php
-                                  $awal[]=$value['Dependency'];
-                                  $tujuan[]=$value['ID'];
-                                  $nama[]=$value['Activity Name'];
-                                  if ($value['Dependency']==0) {
-                                    $start=$value['ID'];
-                                  }
+                              if($value['Activity Name']=='$UNNAMED$ '){
+                                $unname=$value['State'];
+                                $a = substr($unname,0,-5);
+                                $nama[]=$a;
+                              }
+                              else{
+                                $nama[]=$value['Activity Name'];
+                              }
+                                $awal[]=$value['Dependency'];
+                                $tujuan[]=$value['ID'];
+                                
+                                if ($value['Dependency']==0) {
+                                  $start=$value['ID'];
+                                }
                               @endphp
                           @endforeach
-
-
+                          </table>
 
                           {{-- Mencari nilai cabang --}}
                           @foreach ($hasil as $key1 => $value1)
@@ -225,21 +250,26 @@
                             @endif
                           @endforeach
                         </div>
-                        <br><br>
+                            <br>
                             <span><h2 style="color:black ">Jalur Uji</h2></span>
                             <div style="color: black">
                               <div id="hasil"></div>
-                              <br><br>
+                              <br>
                               <span><h2 style="color:black ">Keterangan Jalur Uji</h2></span>
                               <div style="color: black">
-  
-                              <div id="hasil-nama"></div>
-                            </div>
-        @endif
-      </div>
-    </div>
-  </div>
-</div>
+                                <div id="hasil-nama"></div>
+                                  <table id="table" class="table table-bordered">
+                                    <tbody>
+                                      <tr>
+                                      </tr>
+                                    </tbody>
+                                  </table>
+                                </div>
+                            @endif
+                        </div>
+                      </div>
+                    </div>
+                  </div>
               </section>
               <script>
                 var awal ={!!json_encode($awal)!!};
@@ -311,13 +341,18 @@
                 var jml_jalur_uji=cabang.length+1;
                 for (var i = 0; i < (jml_jalur_uji); i++) {
                   console.log(cabang);
+                  document.getElementById("table").innerHTML += "<td>";
+
                   for (var j = 0; j < (array.length); j++) {
                     document.getElementById("hasil").innerHTML +=array[j] + ", ";
+
                     for (let k = 0; k < nama.length; k++) {
                       if(array[j]==tujuan[k]){
-                        document.getElementById("hasil-nama").innerHTML +=nama[k] + ", ";
+                        document.getElementById("table").innerHTML += nama[k] + ", ";
                       }
                     }
+                    document.getElementById("table").innerHTML += "</td>";
+
                     if(cabang.includes(array[j]))
                     {
                       var posisi=j+1;
